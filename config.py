facbc2b00b52e50ba2b54ea008bb6dd7a24c1e83
W. Patrick Jones <wpmjones@gmail.com>
2019-04-12 22:45:13 -0500

    Update to PEP8

--- config.py
+++ config.py
@@ -1,19 +1,21 @@
 import yaml
 from datetime import datetime, timedelta
 
-with open('/home/tuba/config.yaml', 'r') as f:
-  settings = yaml.load(f)
+with open("/home/tuba/config.yaml", "r") as f:
+    settings = yaml.load(f)
 
-with open('/home/tuba/emoji.yaml', 'r') as f:
-  emojis = yaml.load(f)
+with open("/home/tuba/emoji.yaml", "r") as f:
+    emojis = yaml.load(f)
 
-def colorPick(r,g,b):
-  return (r*65536) + (g*256) + b
 
-def botLog(command, request, author, channel, errFlag=0):
-  msg = str(datetime.now() - timedelta(hours=6))[:16] + ' - '
-  if errFlag == 0:
-    msg += f'Printing {command} for {request}. Requested by {author} in {channel}.'
-  else:
-    msg += f'ERROR: User provided an incorrect argument for {command}. Argument provided: {request}. Requested by {author} in {channel}.'
-  return msg
+def color_pick(r, g, b):
+    return (r*65536) + (g*256) + b
+
+
+def bot_log(command, request, author, channel, err_flag=0):
+    msg = str(datetime.now() - timedelta(hours=6))[:16] + " - "
+    if err_flag == 0:
+        msg += f"Printing {command} for {request}. Requested by {author} in {channel}."
+    else:
+        msg += f"ERROR: User provided an incorrect argument for {command}. Argument provided: {request}. Requested by {author} in {channel}."
+    return msg
