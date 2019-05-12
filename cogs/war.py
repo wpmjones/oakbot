4fc785b698da5131d733429410a8e39a35605447
W. Patrick Jones <wpmjones@gmail.com>
2019-05-11 23:20:44 -0500

    auto-add (or remove) Discord war role

--- cogs/war.py
+++ cogs/war.py
@@ -1,8 +1,79 @@
+import discord
 import asyncio
-import traceback
-from datetime import datetime
 from discord.ext import commands
 from config import settings
+
+
+class WarSetup(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def war_roles(self):
+        """ Assign inWar role to those participating in the current war """
+        await self.bot.wait_until_ready()
+        conn = self.bot.db.pool
+        channel = self.bot.get_channel(settings['oakChannels']['testChat'])
+        guild = self.bot.get_guild(settings['discord']['oakGuildId'])
+        war_role = guild.get_role(int(settings['oakRoles']['inwar']))
+        while self == self.bot.get_cog("WarSetup"):
+            try:
+                war = await self.bot.coc_client.get_current_war("#CVCJR89")
+                if war.state in ["preparation", "inWar"]:
+                    # find members in current war and assign role
+                    player_tags = [member.tag[1:] for member in war.members]
+                    sql = (f"SELECT discord_ID, '#' || player_tag as player_tag "
+                           f"FROM rcs_discord_links "
+                           f"WHERE player_tag = ANY($1)")
+                    rows = await conn.fetch(sql, player_tags)
+                    names = []
+                    for row in rows:
+                        is_user, user = is_discord_user(guild, int(row['discord_id']))
+                        print(user.display_name)
+                        if not is_user:
+                            self.bot.logger.error(f"Not a valid Discord ID\n"
+                                                  f"Player Tag: {row['player_tag']}\n"
+                                                  f"Discord ID: {row['discord_id']}\n")
+                            continue
+                        await user.add_roles(war_role, reason="Auto add role for war.")
+                        names.append(user.display_name)
+                    sleep_time = 172800
+                    if names:
+                        embed = discord.Embed(title="War roles added", color=discord.Color.red())
+                        embed.add_field(name="Members in War", value="\n".join(names), inline=False)
+                        await channel.send(embed=embed)
+                        self.bot.logger.info("inWar role added automatically")
+                    else:
+                        self.bot.logger.warning("No players found in names list")
+                else:
+                    sql = (f"SELECT discord_ID, '#' || player_tag as player_tag "
+                           f"FROM rcs_discord_links")
+                    rows = await conn.fetch(sql)
+                    for row in rows:
+                        is_user, user = is_discord_user(guild, int(row['discord_id']))
+                        if not is_user:
+                            self.bot.logger.error(f"Not a valid Discord ID\n"
+                                                  f"Player Tag: {row['player_tag']}\n"
+                                                  f"Discord ID: {row['discord_id']}\n")
+                            pass
+                        await user.remove_roles(war_role, reason="Auto remove role after end of war.")
+                    sleep_time = 86400
+                    await channel.send("inWar roles removed for all players.")
+                    self.bot.logger.info("inWar role removed automatically")
+                await asyncio.sleep(sleep_time)
+            except:
+                self.bot.logger.exception("War Roles")
+
+
+def is_discord_user(guild, discord_id):
+    try:
+        user = guild.get_member(discord_id)
+        if user is None:
+            return False, None
+        else:
+            return True, user
+    except:
+        return False, None
+
 
 def setup(bot):
     c = WarSetup(bot)
