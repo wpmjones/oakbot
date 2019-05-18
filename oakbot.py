21005cecf6a258bb7d30fa8c98d53fa80676bd69
W. Patrick Jones <wpmjones@gmail.com>
2019-05-18 14:14:02 -0500

    add code for easy swap with LIVE vs DEV

--- oakbot.py
+++ oakbot.py
@@ -8,16 +8,24 @@ from discord.ext import commands
 from config import settings
 from oakdb import OakDB
 
-logger.add("oakbot.log", rotation="100MB", level="INFO")
-logger.info("Starting bot")
+enviro = "LIVE"
+
+if enviro == "LIVE":
+    token = settings['discord']['oakbotToken']
+    prefix = "/"
+    log_level = "INFO"
+else:
+    token = settings['discord']['testToken']
+    prefix = ">"
+    log_level = "DEBUG"
+
+logger.add("oakbot.log", rotation="100MB", level=log_level)
 
 description = """Welcome to The Arborist - by TubaKid
 
 All commands must begin with a slash"""
 
-bot = commands.Bot(command_prefix="/", description=description, case_insensitive=True)
-bot.remove_command("help")
-bot.repo = git.Repo(os.getcwd())
+bot = commands.Bot(command_prefix=prefix, description=description, case_insensitive=True)
 
 
 @bot.event
@@ -29,9 +37,27 @@ async def on_ready():
     await bot.test_channel.send("The Arborist is now planting trees")
 
 
-initialExtensions = ["cogs.general", "cogs.members", "cogs.elder", "cogs.owner"]
+@bot.event
+async def on_resumed(self):
+    logger.info('resumed...')
+
+initialExtensions = ["cogs.general",
+                     "cogs.members",
+                     "cogs.elder",
+                     "cogs.owner",
+                     ]
 
 if __name__ == "__main__":
+    bot.remove_command("help")
+    bot.repo = git.Repo(os.getcwd())
+    bot.db = OakDB(bot)
+    loop = asyncio.get_event_loop()
+    pool = loop.run_until_complete(bot.db.create_pool())
+    bot.loop = loop
+    bot.db.pool = pool
+    bot.logger = logger
+    bot.coc_client = coc.Client(settings['supercell']['user'], settings['supercell']['pass'])
+
     for extension in initialExtensions:
         try:
             bot.load_extension(extension)
@@ -40,10 +66,4 @@ if __name__ == "__main__":
             logger.info(f"Failed to load extension {extension}")
             traceback.print_exc()
 
-bot.db = OakDB(bot)
-loop = asyncio.get_event_loop()
-pool = loop.run_until_complete(bot.db.create_pool())
-bot.db.pool = pool
-bot.logger = logger
-bot.coc_client = coc.Client(settings['supercell']['user'], settings['supercell']['pass'])
-bot.run(settings['discord']['oakbotToken'])
+    bot.run(token)
