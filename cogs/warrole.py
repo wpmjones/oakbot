440cdd508f8213b5c7c5382e0f1e43ce40d398fa
W. Patrick Jones <wpmjones@gmail.com>
2019-08-13 22:23:55 -0400

    working on auto role assignment

--- cogs/warrole.py
+++ cogs/warrole.py
@@ -8,15 +8,18 @@ class WarSetup(commands.Cog):
     """Commands to be run during war"""
     def __init__(self, bot):
         self.bot = bot
-        self.bot.coc_client.add_events(self.on_war_state_change)
         asyncio.ensure_future(self.assign_clans())
+        self.bot.coc_client.add_events(self.on_war_state_change)
         self.bot.coc_client.start_updates("war")
 
     def cog_unload(self):
         self.bot.coc_client.stop_updates("war")
 
     async def assign_clans(self):
-        await self.bot.coc_client.add_war_update("#CVCJR89")
+        try:
+            await self.bot.coc_client.add_war_update("#CVCJR89")
+        except:
+            self.bot.logger.exception("Failed to assign clans")
 
     @property
     def elder_channel(self):
