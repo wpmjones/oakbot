a2c7af86487d149615595a0cd345c13782b98f1b
W. Patrick Jones <wpmjones@gmail.com>
2019-08-15 21:43:02 -0400

    fixing auto war role assignment

--- cogs/warrole.py
+++ cogs/warrole.py
@@ -25,6 +25,7 @@ class WarSetup(commands.Cog):
         guild = self.bot.get_guild(settings['discord']['oakGuildId'])
         war_role = guild.get_role(settings['oakRoles']['inwar'])
         if current_state == "preparation":
+            self.bot.logger.debug("War state changed to preparation")
             player_tags = [member.tag[1:] for member in war.members if not member.is_opponent]
             sql = (f"SELECT discord_ID, '#' || player_tag as player_tag "
                    f"FROM rcs_discord_links "
@@ -57,39 +58,46 @@ class WarSetup(commands.Cog):
             except:
                 self.bot.logger.exception("Send Embed")
         elif current_state == "inWar":
+            self.bot.logger.debug("War state changed to in war")
+            # Remove all roles and re-add to compensate for
             members = war_role.members
-            if not members:
-                player_tags = [member.tag[1:] for member in war.members if not member.is_opponent]
-                sql = (f"SELECT discord_ID, '#' || player_tag as player_tag "
-                       f"FROM rcs_discord_links "
-                       f"WHERE player_tag = ANY($1)")
-                rows = await conn.fetch(sql, player_tags)
-                names = []
-                try:
-                    for row in rows:
-                        is_user, user = is_discord_user(guild, int(row['discord_id']))
-                        if not is_user:
-                            self.bot.logger.error(f"Not a valid Discord ID\n"
-                                                  f"Player Tag: {row['player_tag']}\n"
-                                                  f"Discord ID: {row['discord_id']}\n")
-                            continue
-                        await user.add_roles(war_role, reason="Auto add role for war.")
-                        names.append(user.display_name)
-                except:
-                    self.bot.logger.exception("Failed while adding roles")
-                try:
-                    if names:
-                        embed = discord.Embed(title="War roles added", color=discord.Color.red())
-                        embed.add_field(name="Members in War", value="\n".join(names), inline=False)
-                        hours_left = war.end_time.seconds_until // 3600
-                        minutes_left = (war.end_time.seconds_until - (hours_left * 3600)) // 60
-                        embed.set_footer(text=f"War ends in {hours_left} hours, {minutes_left} minutes.")
-                        await self.elder_channel.send(embed=embed)
-                        self.bot.logger.info("inWar role added automatically")
-                    else:
-                        self.bot.logger.warning("No players found in names list")
-                except:
-                    self.bot.logger.exception("Send Embed")
+            try:
+                for user in members:
+                    await user.remove_roles(war_role, reason="Auto remove role after end of war.")
+            except:
+                self.bot.logger.exception("War Roles")
+            # Re-add roles for current war
+            player_tags = [member.tag[1:] for member in war.members if not member.is_opponent]
+            sql = (f"SELECT discord_ID, '#' || player_tag as player_tag "
+                   f"FROM rcs_discord_links "
+                   f"WHERE player_tag = ANY($1)")
+            rows = await conn.fetch(sql, player_tags)
+            names = []
+            try:
+                for row in rows:
+                    is_user, user = is_discord_user(guild, int(row['discord_id']))
+                    if not is_user:
+                        self.bot.logger.error(f"Not a valid Discord ID\n"
+                                              f"Player Tag: {row['player_tag']}\n"
+                                              f"Discord ID: {row['discord_id']}\n")
+                        continue
+                    await user.add_roles(war_role, reason="Auto add role for war.")
+                    names.append(user.display_name)
+            except:
+                self.bot.logger.exception("Failed while adding roles")
+            try:
+                if names:
+                    embed = discord.Embed(title="War roles added", color=discord.Color.red())
+                    embed.add_field(name="Members in War", value="\n".join(names), inline=False)
+                    hours_left = war.end_time.seconds_until // 3600
+                    minutes_left = (war.end_time.seconds_until - (hours_left * 3600)) // 60
+                    embed.set_footer(text=f"War ends in {hours_left} hours, {minutes_left} minutes.")
+                    await self.elder_channel.send(embed=embed)
+                    self.bot.logger.info("inWar role added automatically")
+                else:
+                    self.bot.logger.warning("No players found in names list")
+            except:
+                self.bot.logger.exception("Send Embed")
         else:
             # refresh role object, pull members with that role, remove the role
             members = war_role.members
