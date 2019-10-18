9a866e7712241272c6091ccadd53067c94b460c9
Patrick Jones <wpmjones@gmail.com>
2019-10-18 10:40:48 -0400

    add admin cog

---
+++ cogs/utils/db.py
@@ -0,0 +1,61 @@
+import asyncpg
+import pymssql
+import time
+
+from datetime import datetime
+from config import settings
+
+
+def conn_sql():
+    conn = pymssql.connect(settings['database']['server'],
+                           settings['database']['username'],
+                           settings['database']['password'],
+                           settings['database']['database'],
+                           autocommit=True)
+    return conn
+
+
+class RcsDB:
+    def __init__(self, bot):
+        self.bot = bot
+
+    @staticmethod
+    async def create_pool():
+        pool = await asyncpg.create_pool(f"{settings['pg']['uri']}/psadmin", max_size=85)
+        return pool
+
+    async def link_user(self, player_tag, discord_id):
+        conn = self.bot.pool
+        sql = f"SELECT discord_id FROM rcs_discord_links WHERE player_tag = '{player_tag}'"
+        row = await conn.fetchrow(sql)
+        if row:
+            if row['discord_id'] == discord_id:
+                # player record is already in db
+                self.bot.logger.debug(f"{player_tag} is already in the database.")
+                return
+            # row exists but has a different discord_id
+            sql = (f"UPDATE rcs_discord_links"
+                   f"SET discord_id = {discord_id}"
+                   f"WHERE player_tag = '{player_tag}'")
+            await conn.execute(sql)
+            self.bot.logger.debug(f"The discord id did not match {discord_id}, so I updated it!")
+            return
+        # no player record in db
+        sql = (f"INSERT INTO rcs_discord_links (discord_id, player_tag) "
+               f"VALUES ({discord_id}, '{player_tag}')")
+        await conn.execute(sql)
+        self.bot.logger.debug(f"{player_tag} added to the database")
+
+    async def last_log(self, log_type):
+        conn = self.bot.pool
+        sql = "SELECT MAX(log_date) as last_log FROM rcs_task_log WHERE log_type = $1"
+        row = await conn.fetch(sql, log_type)
+        if row:
+            return time.strptime(row[0], "%Y-%m-%d")
+        else:
+            return datetime(2019, 1, 1)
+
+    async def get_clans(self):
+        conn = self.bot.pool
+        sql = "SELECT clan_name, clan_tag FROM rcs_clans ORDER BY clan_name"
+        return await conn.fetch(sql)
