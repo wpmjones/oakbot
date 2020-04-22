3e14388533a8b67ce6b9115302d45d579e49c3b9
W. Patrick Jones <wpmjones@gmail.com>
2020-04-21 21:13:57 -0400

    add player and clan converter

--- cogs/utils/converters.py
+++ cogs/utils/converters.py
@@ -1,6 +1,83 @@
 import re
+import coc
+
 from datetime import datetime
+from cogs.utils.constants import clans
 from discord.ext import commands
+
+tag_validator = re.compile("^#?[PLYQGRJCUV0289]+$")
+
+
+class PlayerConverter(commands.Converter):
+    async def convert(self, ctx, argument):
+        if isinstance(argument, coc.BasicPlayer):
+            return argument
+
+        tag = coc.utils.correct_tag(argument)
+        name = argument.strip()
+
+        if tag_validator.match(tag):
+            try:
+                return await ctx.coc.get_player(tag)
+            except coc.NotFound:
+                raise commands.BadArgument("Looks like you provided a tag, but there are no players with that tag. "
+                                           "Care to try again?")
+
+        for clan_tag in clans.values():
+            clan = ctx.coc.get_clan(clan_tag)
+            if clan.name == name or clan.tag == tag:
+                raise commands.BadArgument(f"You appear to be passing the clan tag/name for `{clan.name}`")
+            member = clan.get_member(name=name)
+            if member:
+                return member
+
+        raise commands.BadArgument(f"Invalid tag or IGN. Please try again")
+
+
+class ClanConverter(commands.Converter):
+    async def convert(self, ctx, argument):
+        if isinstance(argument, coc.BasicClan):
+            return argument
+
+        tag = coc.utils.correct_tag(argument)
+        name = argument.strip().lower()
+        clans = {
+            "oak": "CVCJR89",
+            "reddit oak": "CVCJR89",
+            "quercus": "GVCPPG98",
+            "reddit quercus": "GVCPPG98",
+        }
+
+        # If tag is valid, use the tag
+        if tag_validator.match(tag):
+            try:
+                if tag[1:] in clans.values():
+                    clan = await ctx.coc.get_clan(tag)
+                else:
+                    raise commands.BadArgument(f"{tag} is not a valid RCS clan.")
+            except coc.NotFound:
+                raise commands.BadArgument(f"{tag} is not a valid clan tag.")
+
+            if clan:
+                return clan
+
+            raise commands.BadArgument(f'{tag} is not a valid clan tag.')
+
+        # If no valid tag, try working with the name
+        if name in clans.keys():
+            tag = "#" + clans[name]
+        else:
+            raise commands.BadArgument(f"{name} is not a valid RCS clan.")
+
+        try:
+            clan = await ctx.coc.get_clan(tag)
+        except coc.NotFound:
+            raise commands.BadArgument(f"{tag} is not a valid clan tag.")
+
+        if clan:
+            return clan
+
+        raise commands.BadArgument(f'Clan name or tag `{argument}` not found')
 
 
 class DateConverter(commands.Converter):
