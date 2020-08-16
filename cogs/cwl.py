e58f7f04bfa2e695042691e22c6917107beaaf35
Patrick <wpmjones@gmail.com>
2020-08-16 11:49:50 -0700

    add on_roster_change

--- cogs/cwl.py
+++ cogs/cwl.py
@@ -1,19 +1,54 @@
+import coc
 import discord
-import re
 
 from discord.ext import commands
 from cogs.utils.constants import clans
-from cogs.utils.models import WarData
 from io import BytesIO
 from PIL import Image, ImageDraw, ImageFont
-from datetime import datetime, timedelta
-from config import settings, emojis
+from config import settings
+
+
+def breakdown(members, process=None):
+    res = {}
+    for m in members:
+        th = m.town_hall if m.town_hall > 9 else 9
+        if th not in res:
+            res[th] = 0
+        val = 1 if process is None else process(m)
+        res[th] += val
+    return "/".join(f"{res.get(th, 0)}" for th in range(13, 8, -1))
+
+
+superscriptNumbers = u"⁰¹²³⁴⁵⁶⁷⁸⁹"
+
+
+def sup(c):
+    if ord('0') <= ord(c) <= ord('9'):
+        return superscriptNumbers[ord(c) - ord('0')]
+    else:
+        return c
+
+
+def th_super(s):
+    s = str(s)
+    ret = u""
+    for c in s:
+        ret += sup(c)
+    return ret
+
+
+def member_display(member):
+    return f"{member.map_position}. {member.name}{th_super(member.town_hall)}"
 
 
 class Cwl(commands.Cog):
     """War bot commands and setup"""
     def __init__(self, bot):
         self.bot = bot
+        self.bot.coc.add_events(self.on_roster_change)
+
+    def cog_unload(self):
+        self.bot.coc.remove_events(self.on_roster_change)
 
     @commands.group(name="cwl", invoke_without_command=True)
     async def cwl(self, ctx, clan_name: str = "oak"):
@@ -55,7 +90,18 @@ class Cwl(commands.Cog):
         buffer.seek(0)
         await ctx.send(file=discord.File(buffer, filename="cwl.png"))
 
-
+    @coc.WarEvents.members(clans['Reddit Oak'])
+    async def on_roster_change(self, old_war, new_war):
+        member_list = []
+        for member in new_war.opponent.members:
+            member_list.append(member_display(member))
+        elder_channel = self.bot.get_channel(settings['oak_channels']['elder_chat'])
+        old_breakdown = breakdown(old_war.members)
+        new_breakdown = breakdown(new_war.members)
+        content = (f"**CWL Roster Change**\n"
+                   f"Old Breakdown: {old_breakdown}\n"
+                   f"New Breakdown: {new_breakdown}")
+        await elder_channel.send("\n".join(member_list))
 
 
 def setup(bot):
