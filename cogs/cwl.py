73f5b08d5b8fd3737af526e4078f7fd628fb3eb5
Patrick <wpmjones@gmail.com>
2020-09-06 15:38:35 -0700

    add cwl status embed

--- cogs/cwl.py
+++ cogs/cwl.py
@@ -2,10 +2,10 @@ import coc
 import discord
 
 from discord.ext import commands
+from cogs.war import to_time
 from cogs.utils.constants import clans
-from io import BytesIO
-from PIL import Image, ImageDraw, ImageFont
-from config import settings
+from datetime import datetime
+from config import settings, emojis
 
 
 def breakdown(members, process=None):
@@ -62,38 +62,66 @@ class Cwl(commands.Cog):
             clan_tag = clans['Reddit Oak']
         else:
             clan_tag = clans['Reddit Quercus']
-        image = Image.open(f"images/oak-bg-1.jpg")
-        font_24 = ImageFont.truetype("fonts/adam.otf", 24)
-        font_36 = ImageFont.truetype("fonts/adam.otf", 36)
+        war = await self.bot.coc.get_current_war(clan_tag)
+        if not war.is_cwl:
+            return await ctx.send(f"It appears that {war.clan} is not currently involved in CWL.  Please use "
+                                  f"`/war` commands instead.")
+        embed = discord.Embed(title="CWL Status", color=discord.Color.dark_blue())
         group = await self.bot.coc.get_league_group(clan_tag)
-        counter = 1
-        x = 300
-        y = 50
-        draw = ImageDraw.Draw(image)
-        async for war in group.get_wars():
-            draw.rectangle([x, y, x+650, y+50], fill=(15, 15, 15))
-            draw.text((x+10, y+12), f"{counter}. {war.clan.name}", fill=(250, 250, 250), font=font_36)
-            draw.rectangle([x+700, y, x+1000, y+50], fill=(15, 15, 15))
-            # Center
-            text_width, text_height = draw.textsize(f"stars", font_36)
-            center_x = (x + 850) - (text_width / 2)
-            draw.text((center_x, y+12), f"stars", fill=(250, 250, 250), font=font_36)
-            draw.rectangle([x+1050, y, x+1300, y+50], fill=(15, 15, 15))
-            # Center
-            text_width, text_height = draw.textsize(f"dest", font_36)
-            center_x = (x + 1175) - (text_width / 2)
-            draw.text((center_x, y+12), f"dest", fill=(250, 250, 250), font=font_36)
+        num_rounds = len(group.rounds) - 1  # API always includes the next round which we don't need
+        print(num_rounds)
+        counter = 0
+        content = ""
+        while counter < num_rounds:
+            async for cwl_war in group.get_wars(counter):
+                finished_status = ("won", "tied", "lost")
+                if cwl_war.clan.tag == "#CVCJR89" and cwl_war.status in finished_status:
+                    prefix = cwl_war.status.title()
+                    content += (f"{prefix} - {cwl_war.clan} ({cwl_war.clan.stars}) vs "
+                                f"{cwl_war.opponent} ({cwl_war.opponent.stars})\n")
+                    break
+                if cwl_war.opponent.tag == "#CVCJR89" and cwl_war.status in finished_status:
+                    statuses = {
+                        "won": "Lost",
+                        "winning": "Losing",
+                        "tie": "Tied",
+                        "tied": "Tied",
+                        "lost": "Won",
+                        "losing": "Winning",
+                    }
+                    prefix = statuses.get(cwl_war.status, "Unknown")
+                    content += (f"{prefix} - {cwl_war.opponent} ({cwl_war.opponent.stars}) vs "
+                                f"{cwl_war.clan} ({cwl_war.clan.stars})\n")
+                    break
             counter += 1
-            y += 65
-        buffer = BytesIO()
-        image.save(buffer, "png")
-        buffer.seek(0)
-        await ctx.send(file=discord.File(buffer, filename="cwl.png"))
+        embed.add_field(name="Previous Rounds", value=content)
+        blank = emojis['other']['gap']
+        th_list = (f"{emojis['th_icon'][13]} {emojis['th_icon'][12]} {emojis['th_icon'][11]} {emojis['th_icon'][10]} "
+                   f"{emojis['th_icon'][9]}")
+        now = datetime.utcnow()
+        if now < war.start_time.time:
+            time_calc = f"Prep day. {to_time(war.start_time.seconds_until)} until war starts."
+        else:
+            time_calc = f"War ends in {to_time(war.end_time.seconds_until)}"
+        embed.set_thumbnail(url="https://vignette.wikia.nocookie.net/clashofclans/images/9/97/LeagueMedal.png")
+        embed.add_field(name=f"{war.clan.name} vs {war.opponent.name}", value=time_calc, inline=False)
+        embed.add_field(name=war.clan.name,
+                        value=(f"{war.clan.stars} of {war.clan.max_stars} Stars\n"
+                               f"{war.clan.destruction:.1f}% Destruction"),
+                        inline=True)
+        embed.add_field(name=war.opponent.name,
+                        value=(f"{war.opponent.stars} of {war.opponent.max_stars} Stars\n"
+                               f"{war.opponent.destruction:.1f}% Destruction"),
+                        inline=True)
+        embed.add_field(name=blank, value=blank, inline=True)
+        await ctx.send(embed=embed)
 
     @coc.WarEvents.members(clans['Reddit Oak'])
     async def on_roster_change(self, old_war, new_war):
+        self.bot.logger.info("Roster change detected in CWL")
         member_list = []
         for member in new_war.opponent.members:
+            print(member_display(member))
             member_list.append(member_display(member))
         elder_channel = self.bot.get_channel(settings['oak_channels']['elder_chat'])
         old_breakdown = breakdown(old_war.members)
